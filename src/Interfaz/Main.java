02598b2d04fdf2e019fbf7741ccec3ef72514b12
vicente132 <vihernandez2022@udec.cl>
2023-11-02 17:04:23 -0300

    fix
    ordenar un poco el main para que tenga mas sentido

--- src/Interfaz/Main.java
+++ src/Interfaz/Main.java
@@ -8,17 +8,21 @@ import javax.swing.*;
 
 public class Main{
     public static void main(String[] Args) throws Exception {
-        int a=0;
+        Moneda m = new Moneda1500();
+        Moneda m1 = new Moneda1500();
+
+        Expendedor e = new Expendedor(1);
         PanelComprador n =new PanelComprador();
         PanelExpendedor e1=new PanelExpendedor();
         PanelPrincipal p =new PanelPrincipal(n,e1);
 
-        Moneda m = new Moneda1500();
-        Moneda m1 = new Moneda1500();
-        Expendedor e = new Expendedor(1);
+        int seleccion=e1.getSeleccion();
 
-        Comprador d = new Comprador(m, e1.getSeleccion(), e);
-        
+
+
+
+        Comprador d = new Comprador(m, seleccion, e);
+
 
 
 
