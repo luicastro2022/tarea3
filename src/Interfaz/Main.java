dd4988a7dde41e3a7c6cc776f3f5596ff3a5a3d7
vicente132 <vihernandez2022@udec.cl>
2023-11-02 10:08:09 -0300

    Fix cosas de prueba
    Borre lineas de codigo que eran solo de prueba

--- src/Interfaz/Main.java
+++ src/Interfaz/Main.java
@@ -1,14 +1,12 @@
 package Interfaz;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
 
 public class Main{
     public static void main(String[] Args){
 
-       
+        
+
     }
 
 }
