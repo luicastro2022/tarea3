e0c3b663b8d1e4dea7000738e97cc2cab503b0ea
vicente132 <vihernandez2022@udec.cl>
2023-11-02 11:29:39 -0300

    feat
    Botones del panel comprador con su respectivo listener

--- src/Interfaz/PanelExpendedor.java
+++ src/Interfaz/PanelExpendedor.java
@@ -4,32 +4,36 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-public class PanelExpendedor extends JPanel implements MouseListener{
-
-    public PanelExpendedor(){
+public class PanelExpendedor extends JPanel{
+    
+    public PanelExpendedor() {
         super();
-        this.setLayout(new GridLayout(3,2));
+        this.setLayout(new GridLayout(3, 2));
 
-        JButton cocacola=new JButton("CocaCola");
+        JButton cocacola = new JButton("CocaCola");
         this.add(cocacola);
-        JButton snicker=new JButton("Snicker");
+
+        JButton snicker = new JButton("Snicker");
         this.add(snicker);
-        JButton sprite=new JButton("Sprite");
+        JButton sprite = new JButton("Sprite");
         this.add(sprite);
-        JButton super8=new JButton("Super 8");
+        JButton super8 = new JButton("Super 8");
         this.add(super8);
-        JButton fanta=new JButton("Fanta");
+        JButton fanta = new JButton("Fanta");
         this.add(fanta);
 
-        this.addMouseListener(escuchar);
 
-        this.setSize(800,600);
-        this.setVisible(true);
+        this.addMouseListener(new escucharmouse());
+
+        this.setSize(800, 600);
+
     }
 
-    private MouseListener escuchar=new MouseListener() {
+    private class escucharmouse implements MouseListener{
+
         @Override
         public void mouseClicked(MouseEvent e) {
+            System.out.println("hola");
 
         }
 
@@ -52,29 +56,5 @@ public class PanelExpendedor extends JPanel implements MouseListener{
         public void mouseExited(MouseEvent e) {
 
         }
-    }
-    @Override
-    public void mouseClicked(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-
     }
 }
