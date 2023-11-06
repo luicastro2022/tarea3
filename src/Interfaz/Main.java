6a0bac38572bd3ebb075caa8b3b99e87b049b9a2
vicente132 <vihernandez2022@udec.cl>
2023-11-06 17:17:17 -0300

    feat
    Se eliminaron todas las clases de listeners especificos para cada boton, se añadio a cada boton un listener generico

--- src/Interfaz/Main.java
+++ src/Interfaz/Main.java
@@ -5,10 +5,9 @@ import javax.swing.*;
 
 public class Main{
     public static void main(String[] Args) throws Exception {
-        Moneda m = new Moneda1500();
-        Moneda m1 = new Moneda500();
 
-        Expendedor e = new Expendedor(1);
+        Expendedor expendedor=new Expendedor(1);
+
         PanelComprador n =new PanelComprador();
         PanelExpendedor e1=new PanelExpendedor();
         PanelPrincipal p =new PanelPrincipal(n,e1);
--- src/Interfaz/PanelExpendedor.java
+++ src/Interfaz/PanelExpendedor.java
@@ -1,5 +1,4 @@
 package Interfaz;
-
 import Codigo.*;
 import javax.swing.*;
 import java.awt.*;
@@ -20,26 +19,57 @@ public class PanelExpendedor extends JPanel{
         super();
         this.setLayout(new GridLayout(3, 2));
 
+
         JButton cocacola = new JButton("CocaCola");
         this.add(cocacola);
-        cocacola.addMouseListener(new EscucharCocacola());
+        cocacola.addMouseListener(new EscucharMouse(){
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                seleccion=1;
+                System.out.println(seleccion);
+            }
+        });
 
 
         JButton sprite = new JButton("Sprite");
         this.add(sprite);
-        sprite.addMouseListener(new EscucharSprite());
+        sprite.addMouseListener(new EscucharMouse(){
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                seleccion=2;
+                System.out.println(seleccion);
+            }
+        });
 
         JButton fanta = new JButton("Fanta");
         this.add(fanta);
-        fanta.addMouseListener(new EscucharFanta());
+        fanta.addMouseListener(new EscucharMouse(){
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                seleccion=3;
+                System.out.println(seleccion);
+            }
+        });
 
         JButton snicker = new JButton("Snicker");
         this.add(snicker);
-        snicker.addMouseListener(new EscucharSnicker());
+        snicker.addMouseListener(new EscucharMouse(){
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                seleccion=4;
+                System.out.println(seleccion);
+            }
+        });
 
         JButton super8 = new JButton("Super 8");
         this.add(super8);
-        super8.addMouseListener(new EscucharSuper8());
+        super8.addMouseListener(new EscucharMouse(){
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                seleccion=5;
+                System.out.println(seleccion);
+            }
+        });
 
 
         this.setSize(800, 600);
